2e92ef3bcab2621712af4c3d93e565847fe7d2dc
paramprashar1 <param.prashar24@gmail.com>
2023-01-08 11:47:56 +0530

    deleted sb

--- src/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://ugdwmxrabvmybuntigbx.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnZHdteHJhYnZteWJ1bnRpZ2J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIyNDExNzcsImV4cCI6MTk4NzgxNzE3N30.yCoN9IQAftZPXxAX1e9FUXn6yWTZlSqVvW-9Iab0fE4";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
-var tes = 1;
